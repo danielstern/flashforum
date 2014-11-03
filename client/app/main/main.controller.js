f80b74ba04fa1a8613538bc40b7503a2e01352c0
Daniel Stern <daniel@danielstern.ca>
2014-11-02 22:00:36 -0800

    awesome

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,19 +1,27 @@
 'use strict';
 
 angular.module('flashforumApp')
-  .controller('MainCtrl', function ($scope, $http, socket) {
+  .controller('MainCtrl', function ($scope, $http, socket,Auth) {
     $scope.awesomeThings = [];
+
+    $scope.thread = {
+      title: "Which decks do you think will be popular in the PTQ this year?",
+      description: "I'm just wondering because my deck cost $400 and I'm concerned my heat may be shut off."
+    }
 
     $http.get('/api/things').success(function(awesomeThings) {
       $scope.awesomeThings = awesomeThings;
+      console.log("awesomeThings?",awesomeThings);
       socket.syncUpdates('thing', $scope.awesomeThings);
     });
+
+    console.log("user",Auth.getCurrentUser());
 
     $scope.addThing = function() {
       if($scope.newThing === '') {
         return;
       }
-      $http.post('/api/things', { name: $scope.newThing });
+      $http.post('/api/things', { name: $scope.newThing, owner:Auth.getCurrentUser() });
       $scope.newThing = '';
     };
 
