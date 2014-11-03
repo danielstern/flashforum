9b76f76ab0eb86293615d1b7d48dbf01e7a7cfd5
Daniel Stern <daniel@danielstern.ca>
2014-11-03 01:31:22 -0800

    liveeditfail

--- client/app/thread/thread.controller.js
+++ client/app/thread/thread.controller.js
@@ -9,25 +9,31 @@ angular.module('flashforumApp')
     });
 
     $scope.$watch("threads",function(threads){
-      console.log("thread update...");
-      return;
       if (!threads) return;
-      var thread = threads[0];
-      var timeNow = new Date().getTime();
-      if (!$scope.thread || thread.ts > $scope.thread.ts) {
-        $scope.thread = thread;
-      }
+      $scope.thread = threads[0];
+      // if (!$scope.thread || thread.ts > $scope.thread.ts) {
+        // $scope.thread = thread;
+        // $scope.lastUpdated = true;
+      // }
     },true);
 
     $http.get('/api/posts/thread/'+$stateParams.name).success(function(posts) {
-      console.log("Got posts...",posts);      $scope.posts = posts;
+      $scope.posts = posts;
       socket.syncUpdates('post', $scope.posts);
     });
 
-    $scope.$watch('thread',function(thread,oldThread){
-      if (thread === oldThread) return;
-      thread.ts = new Date().getTime();
-      $http.patch('api/threads/'+thread._id,thread);
+    $scope.updateThread = function() {
+      $http.patch('api/threads/'+$scope.thread._id,$scope.thread);
+    }
+
+    $scope.$watch("thread",function(thread){
+      // if ($scope.lastUpdated) {
+        // $scope.lastUpdated = false;
+        // return;
+      // }
+      if (!thread) return;
+      // thread.ts = new Date().getTime();
+      
     },true);
 
     $scope.addPost = function() {
